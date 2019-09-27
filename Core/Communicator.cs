9b2132909e35dc574c80ba4ff415f41308ddda35
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-09-27 15:32:04 +0200

    Update Communicator.cs

--- Core/Communicator.cs
+++ Core/Communicator.cs
@@ -33,8 +33,7 @@ namespace Core
 
         public List<string> Receiver()
         {
-            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
-            using IConnection connection = factory.CreateConnection();
+            using IConnection connection = new ConnectionFactory() { HostName = "localhost" }.CreateConnection();
             using IModel channel = connection.CreateModel();
             channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
