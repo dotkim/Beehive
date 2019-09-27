3bd1281631bcf5ecf2faf55ec6a77b8655e3b849
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-09-27 15:31:23 +0200

    push

---
+++ Core/Communicator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Core
+{
+    public class Communicator
+    {
+        public string Send(string message)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.QueueDeclare(
+                queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            byte[] body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+              exchange: "",
+              routingKey: "hello",
+              basicProperties: null,
+              body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+            return message;
+        }
+
+        public List<string> Receiver()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            List<string> messages = new List<string>();
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body;
+                string message = Encoding.UTF8.GetString(body);
+                messages.Add(message);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+            return messages;
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Core;
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Communicator com = new Communicator();
+            com.Send("Hello World!");
+            List<string> messages = com.Receiver();
+            Console.WriteLine(messages.Count);
+        }
+    }
+}
