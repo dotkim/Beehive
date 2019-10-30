27c2dbeed3db6558a0268d381b57b4891ec0d8d0
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-10-30 12:36:58 +0100

    New project files and rename solution

---
+++ Registry/Registry.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using Core;
+
+
+namespace Registry
+{
+    public class ExThread
+    {
+        private Communicator com = new Communicator();
+
+        // Non-static method 
+        public void mythread1()
+        {
+            while (true)
+            {
+                Thread.Sleep(10000);
+                com.Send("testmelding :)");
+            }
+        }
+
+        public void mythread2()
+        {
+            com.Receiver();
+        }
+    }
+
+    class Registry
+    {
+        static void Main(string[] args)
+        {
+            ExThread obj = new ExThread();
+            Thread thr = new Thread(obj.mythread1);
+            Thread thr2 = new Thread(obj.mythread2);
+            thr.Start();
+            thr2.Start();
+        }
+    }
+}
