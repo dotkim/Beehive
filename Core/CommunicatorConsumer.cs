b342424c6886275ecf20b116c02be408e4ede7e7
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-12-19 15:24:50 +0100

    comments

--- Core/CommunicatorConsumer.cs
+++ Core/CommunicatorConsumer.cs
@@ -2,8 +2,15 @@
 
 namespace Core
 {
+    /// <summary>
+    /// Communicator consumer class
+    /// </summary>
     class CommunicatorConsumer
     {
+        /// <summary>
+        /// Creates a new consumer.
+        /// </summary>
+        /// <returns>consumer of typeEventingBasicConsumer</returns>
         public static EventingBasicConsumer CreateConsumer()
         {
             return new EventingBasicConsumer(CommunicatorFactory.GetChannel());
--- Core/Configuration.cs
+++ Core/Configuration.cs
@@ -2,14 +2,28 @@
 
 namespace Core
 {
+    /// <summary>
+    /// Configuration class.
+    /// Sets the default and/or provided configuration settings.
+    /// </summary>
     public class Configuration
     {
+        /// <value>The RabbitMQ host URI.</value>
         public string RmqUri { get; set; } = "localhost";
+
+        /// <value>RabbitMQ Port.</value>
         public int RmqPort { get; set; } = 5672;
+
+        /// <value>RabbitMQ username.</value>
         public string RmqUserName { get; set; } = "guest";
+
+        /// <value>RabbitMQ password.</value>
         public string RmqPassword { get; set; } = "guest";
+
+        /// <value>The applications RabbitMQ queue.</value>
         public string RmqQueue { get; set; } = "Default";
 
+        /// <value>Application name, should always be set.</value>
         public string ApplicationName { get; set; } = "DefaultApplication";
 
         public List<string> SubscribedFields { get; set; } = new List<string>();
